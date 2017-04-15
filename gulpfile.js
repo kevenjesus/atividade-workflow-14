970839b076da42358d164204735a5a015f7959f0
Keven Jesus <falecom@keven.com.br>
2017-04-15 14:46:23 -0300

    modificações nas tasks com os plugins para efetuar as tarefas, renome na task estilo para escutar (semantica e sentido)

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var htmlmin = require("gulp-htmlmin");
+
 
 //Copiando todo conteudo da pasta scss para pasta Dist/Css
 
-var gulp = require("gulp");
+
  gulp.task('move-scss',function (){
  	return gulp.src('./source/scss/*.scss')
+ 			 .pipe(sass())
  		   .pipe(gulp.dest('./dist/css')); 	
  });
 
 //Enviando o conteudo html da pasta source para pasta Dist
 
- var gulp = require("gulp");
  gulp.task('move-html',function (){
  	return gulp.src('./source/*.html')
+ 			 .pipe(htmlmin({collapseWhitespace: true}))
  		   .pipe(gulp.dest('./dist')); 	
  });
 
  //Usando o comando Task para escutar os arquivos selecionados
 
-gulp.task('estilo',function(){
+gulp.task('escutar',function(){
 	gulp.watch('./source/scss/*.scss',['move-scss']);
-	gulp.watch('./source/scss/*.html',['move-scss']);
+	gulp.watch('./source/scss/*.html',['move-html']);
 });
